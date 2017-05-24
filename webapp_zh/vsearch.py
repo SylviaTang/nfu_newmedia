a6ac45d47e9c3ee175fa6d7bc2c33f86f3d6b248
SylviaTang <948638795@qq.com>
2017-05-24 14:36:43 +0800

    更改变数名vowels-->PREPOSITION_zhs
    更改名称

--- webapp_zh/vsearch.py
+++ webapp_zh/vsearch.py
@@ -1,10 +1,10 @@
-
+# -*- coding: utf-8 -*-
 def search4prepositions_zhs(phrase: str) -> set:
     """Return any prepositions found in a supplied phrase."""
     # Source http://xh.5156edu.com/page/z3033m4876j18636.html
     # Source https://resources.allsetlearning.com/chinese/grammar/Preposition
-    vowels = set('aeiou')
-    return vowels.intersection(set(phrase))
+    PREPOSITION_zhs = set('aeiou')
+    return PREPOSITION_zhs.intersection(set(phrase))
 
 def search4letters(phrase: str, letters: str='aeiou') -> set:
     """Return a set of the 'letters' found in 'phrase'."""
